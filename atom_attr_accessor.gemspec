b173cf9ba1f347b7ed957c6754d80bef76cc95d8
Fernando Ipar <fipar@acm.org>
2014-03-17 16:18:08 -0300

    Initial Commit

---
+++ atom_attr_accessor.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'atom_attr_accessor'
+  s.version     = '0.1.0'
+  s.date        = '2014-03-17'
+  s.summary     = "Defines accessor methods using clojure atoms"
+  s.description = "Defines accessor methods for instance variables using clojure's atoms, for thread safety"
+  s.authors     = ["Fernando Ipar"]
+  s.email       = 'fipar@acm.org'
+  s.files       = ["lib/atom_attr_accessor.rb", "lib/clojure-1.5.1.jar"]
+  s.homepage    =
+    'http://rubygems.org/gems/atom_attr_accessor'
+  s.license       = 'EPL-1.0'
+end
---
+++ lib/atom_attr_accessor.rb
@@ -0,0 +1,53 @@
+# example use: 
+
+# class ThreadSafeVar
+#   atom_attr_accessor :id, :name
+# end
+
+# v = ThreadSafeVar.new
+# v.id = 1
+# v.name = "test"
+# puts "meet v: "
+# puts v.id
+# puts v.name
+
+# puts "now let's fire up concurrent threads to change v.id, and check its value every second while they run"
+
+# Thread.new {v.id = v.id + 10; puts "thread 1 done"}
+# Thread.new {v.id = v.id + 1; puts "thread 2 done"}
+# Thread.new {v.id = v.id + 5; puts "thread 3 done"}
+
+# # give them some time
+# sleep 2
+
+# puts "new value: #{v.id}"
+
+class Class
+  def atom_attr_accessor(*args)
+    self.class_eval("
+require 'java'
+require 'clojure-1.5.1.jar'
+java_import 'clojure.lang.LockingTransaction'
+java_import 'clojure.lang.Atom'
+")
+
+    args.each do |arg|
+      self.class_eval("
+def #{arg}
+   @#{arg}.deref unless @#{arg}.nil? 
+end
+
+def #{arg}=(val)
+   if @#{arg}.nil?
+      @#{arg} = Atom.new(val) 
+   else
+      @#{arg}.reset val
+   end
+end
+")
+      
+    end 
+  end
+end
+
+
